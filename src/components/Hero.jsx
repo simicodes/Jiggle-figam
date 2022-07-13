6c570b48e36f2955d2a9eac308e62a2fbb637a34
simicodes <simeonoffiong@gmail.com>
2022-07-13 07:34:19 -0600

    Jiggle-Figma

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import bgImg from "../assets/bg.png";
+const Hero = () => {
+  return (
+    <div className="w-full h-screen relative">
+      <img className="w-full h-full object-cover" src={bgImg} alt="BGimg" />;
+      {/* OverLay........ */}
+      <div className=" absolute w-full h-full top-0 left-0 bg-gray-900/70"></div>
+      {/* Content........ */}
+      <div className="absolute  w-[ 627px] h-[140px] top-[370px] flex flex-col justify-center left-[15px]  md:left-[84px]  text-white ">
+        <h1 className="font-[600] md:font-bold md:leading-[70px] leading-[40px] text-[30px] md:text-[55px] not-italic">
+          Introducing Jiggle
+        </h1>
+        <h1 className="font-[600] md:font-bold md:leading-[70px] leading-[40px] text-[30px] md:text-[55px] not-italic">
+          Food insurance for Students
+        </h1>
+        <p className=" pb-8 font-[200] md:font-[400] text-[20px] md:text-[48px] leading-[40px]">
+          Pay once , eat wherever and whenever
+        </p>
+        <div>
+          <button className="btn1 m-2 font-[500] text-[#1EA6CC]">
+            Sign Up Now
+          </button>
+          <button className=" btn2 m-2 font-[600]">Become a Vendor</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+//import { Link } from "react-router-dom";
+
+import Logo from "../assets/logo.png";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleToggleMenu = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  //const handleClose = () => setToggleMenu(!toggleMenu);
+
+  return (
+    <div className="w-full h-[90px] cursor-pointer absolute z-10 text-white">
+      <div className="max-w-[1240px]  mx-auto px-4 flex justify-between items-center h-full">
+        <div className="z-10">
+          <img className="w-[195px] h-[65px]" src={Logo} alt="/" />
+        </div>
+        <div className="hidden md:flex">
+          <ul className="text-white flex items-center">
+            <li>Home</li>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Blog</li>
+            <li className="login border border-[#1EA6CC] text-[#1EA6CC]">
+              Login
+            </li>
+          </ul>
+        </div>
+        {/* Hamburger Menu */}
+        <div onClick={handleToggleMenu} className="block md:hidden">
+          {toggleMenu ? (
+            <AiOutlineClose size={30} className="text-white" />
+          ) : (
+            <AiOutlineMenu size={30} className="text-white" />
+          )}
+        </div>
+        {/* Mobile Menu */}
+        <div
+          className={
+            toggleMenu
+              ? "w-full h-full  text-white absolute top-[90px] left-0 flex justify-center text-center"
+              : "absolute left-[-100%]"
+          }
+        >
+          <ul>
+            <li>Home</li>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Blog</li>
+            <li className="login border border-[#1EA6CC] text-[#1EA6CC]">
+              Login
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
